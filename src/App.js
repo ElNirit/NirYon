0a4b8fd931cc98e23153ddfa26a7459f2e0b180e
ElNirit <elnirit@gmail.com>
2021-08-03 19:59:32 +0300

    Beginning the project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import './App.css';
+import { Button, Card, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
