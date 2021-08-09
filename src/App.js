91cbe1e01fa30bd975e82369a4c4b3e66f3f5faa
ElNirit <elnirit@gmail.com>
2021-08-09 10:35:29 +0300

    App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 import { Button, Card, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Parse from 'parse/react-native';
 
 function App() {
   
   return (
-    <div className="App">
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a href="">NirYon</a>
+        </div>
+        <div>
+          <a href="">Cart</a>
+          <a href="">Sign in</a>
+        </div>
+      </header>
      
     </div>
   );
