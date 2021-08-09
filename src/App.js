9170617169e11420e634c2e67e197b5404310b21
ElNirit <elnirit@gmail.com>
2021-08-09 20:56:13 +0300

    adding product as a compnent

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Button, Card, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Parse from 'parse/react-native';
+import Product from "./components/Product";
 
 function App() {
   
@@ -11,11 +12,11 @@ function App() {
     <div className="grid-container">
       <header className="row">
         <div>
-          <a href="">NirYon Logo</a>
+          <a className="brand" href="/">NirYon Logo</a>
         </div>
         <div>
-          <a href="">לוגו סל קניות</a>
-          <a href="">התחברות / הרשמה</a>
+          <a href="/cart">לוגו סל קניות</a>
+          <a href="/signin">התחברות / הרשמה</a>
         </div>
       </header>
       <main>
@@ -23,24 +24,7 @@ function App() {
          <div>
           <div className="row center">
             {data.products.map(product => (
-            <div key={product.id} className="card">
-              <a href={'/product/${product.id}'}>
-                <img className="medium" src={product.image} alt={product.name} />
-              </a>
-              <div className="card-body">
-                <a href={'/product/${product.id}'}>
-                  <h2>{product.name}</h2>
-                </a>
-                {/* <div className="rating">
-                  <span> <i className="fa fa-star"></i> </span>
-                  <span> <i className="fa fa-star"></i> </span>
-                  <span> <i className="fa fa-star"></i> </span>
-                  <span> <i className="fa fa-star"></i> </span>
-                  <span> <i className="fa fa-star"></i> </span>
-                </div> */}
-                <div className="price">{product.price} ש"ח</div>
-              </div>
-            </div> 
+              <Product key={product.id} product={product}></Product>
             ))}           
           </div>
         </div>
