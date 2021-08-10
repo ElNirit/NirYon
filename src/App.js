2132d0a11e69f3d5572ee5c209e09a56ee83084e
ElNirit <elnirit@gmail.com>
2021-08-10 20:50:01 +0300

    trying again, adding JSON

--- src/App.js
+++ src/App.js
@@ -1,35 +1,139 @@
-import React, { useState, useEffect } from "react";
-import { Button, Card, Form } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import Parse from 'parse/react-native';
-import { BrowserRouter, Route } from "react-router-dom";
-import ProductPage from "./Page/ProductPage";
-import HomePage from "./Page/HomePage";
+import React, { Component } from "react";
+import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
 
-function App() {
-  
-  return (
-  <BrowserRouter>
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">NirYon Logo</a>
-        </div>
-        <div>
-          <a href="/cart">לוגו סל קניות</a>
-          <a href="/signin">התחברות / הרשמה</a>
-        </div>
-      </header>
-      <main>
-        <Route path="/product/:id" component={ProductPage}></Route>
-        <Route path="/" component={HomePage} exact></Route>
-         
-      </main>
-      <footer className="row center">All right reserved</footer>
-    </div>
-  </BrowserRouter>
-  );
+import AddProduct from './components/AddProduct';
+import Cart from './components/Cart';
+import Login from './components/Login';
+import ProductList from './components/ProductList';
+
+import Context from "./Context";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      cart: {},
+      products: []
+    };
+    this.routerRef = React.createRef();
+  }
+
+  render() {
+    return (
+      <Context.Provider
+        value={{
+          ...this.state,
+          removeFromCart: this.removeFromCart,
+          addToCart: this.addToCart,
+          login: this.login,
+          addProduct: this.addProduct,
+          clearCart: this.clearCart,
+          checkout: this.checkout
+        }}
+      >
+        <Router ref={this.routerRef}>
+        <div className="App">
+          <nav
+            className="navbar container"
+            role="navigation"
+            aria-label="main navigation"
+          >
+            <div className="navbar-brand">
+              <b className="navbar-item is-size-4 ">ניריון</b>
+              <label
+                role="button"
+                className="navbar-burger burger"
+                aria-label="menu"
+                aria-expanded="false"
+                data-target="navbarBasicExample"
+                onClick={e => {
+                  e.preventDefault();
+                  this.setState({ showMenu: !this.state.showMenu });
+                }}
+              >
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+              </label>
+            </div>
+              <div className={`navbar-menu ${
+                  this.state.showMenu ? "is-active" : ""
+                }`}>
+                <Link to="/products" className="navbar-item">
+                  מוצרים
+                </Link>
+                {this.state.user && this.state.user.accessLevel < 1 && (
+                  <Link to="/add-product" className="navbar-item">
+                    הוספת מוצר
+                  </Link>
+                )}
+                <Link to="/cart" className="navbar-item">
+                  סל קניות
+                  <span
+                    className="tag is-primary"
+                    style={{ marginLeft: "5px" }}
+                  >
+                    { Object.keys(this.state.cart).length }
+                  </span>
+                </Link>
+                {!this.state.user ? (
+                  <Link to="/login" className="navbar-item">
+                    התחברות
+                  </Link>
+                ) : (
+                  <Link to="/" onClick={this.logout} className="navbar-item">
+                    יציאה
+                  </Link>
+                )}
+              </div>
+            </nav>
+            <Switch>
+              <Route exact path="/" component={ProductList} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/cart" component={Cart} />
+              <Route exact path="/add-product" component={AddProduct} />
+              <Route exact path="/products" component={ProductList} />
+            </Switch>
+          </div>
+        </Router>
+      </Context.Provider>
+    );
+  }
 }
 
-export default App;
+// import React, { useState, useEffect } from "react";
+// import { Button, Card, Form } from 'react-bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+// import Parse from 'parse/react-native';
+// import { BrowserRouter, Route } from "react-router-dom";
+// import ProductPage from "./Page/ProductPage";
+// import HomePage from "./Page/HomePage";
+
+// function App() {
+  
+//   return (
+//   <BrowserRouter>
+//     <div className="grid-container">
+//       <header className="row">
+//         <div>
+//           <a className="brand" href="/">NirYon Logo</a>
+//         </div>
+//         <div>
+//           <a href="/cart">לוגו סל קניות</a>
+//           <a href="/signin">התחברות / הרשמה</a>
+//         </div>
+//       </header>
+//       <main>
+//         <Route path="/product/:id" component={ProductPage}></Route>
+//         <Route path="/" component={HomePage} exact></Route>
+         
+//       </main>
+//       <footer className="row center">All right reserved</footer>
+//     </div>
+//   </BrowserRouter>
+//   );
+// }
+
+// export default App;
