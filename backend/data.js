3d957afdfd8a78a8ece4c3a8d83ed8c9c50162d5
ElNirit <elnirit@gmail.com>
2021-08-12 23:23:08 +0300

    proxy doesn't work

---
+++ backend/data.js
@@ -0,0 +1,60 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name: 'פינגר ליים ירוק',
+            catagory: 'פירות הדר',
+            image:'/images/fingerLimeGreen.jpg',
+            price: 19,
+            owner:'ניריון',
+            description:''
+        },
+        {
+            _id:'2',
+            name: 'פינגר ליים אדום',
+            catagory: 'פירות הדר',
+            image:'/images/red-sampanya-finger-lime.jpg',
+            price: 19,
+            owner:'ניריון',
+            description:''
+        },
+        {
+            _id:'3',
+            name: 'פינגר ליים צהוב',
+            catagory: 'פירות הדר',
+            image:'/images/sari-finger-lime.jpg',
+            price: 19,
+            owner: ' ניריון',
+            description:''
+        },
+        {
+            _id:'4',
+            name: 'לימון מאייר',
+            catagory: 'פירות הדר',
+            image:'/images/meyer-limon.jpg',
+            price: 19,
+            owner:'ניריון',
+            description:''
+        },
+        {
+            _id:'5',
+            name: 'לימון קאפיר',
+            catagory: 'פירות הדר',
+            image:'/images/kaffir-lime.jpg',
+            price: 19,
+            owner:'ניריון',
+            description:''
+        },
+        {
+            _id:'6',
+            name: 'אצבעות בודהה',
+            catagory: 'פירות הדר',
+            image:'/images/budanin-lemon.jpg',
+            price: 19,
+            owner:'ניריון',
+            description:''
+        }
+    ]
+};
+
+export default data;
--- src/Page/HomePage.js
+++ src/Page/HomePage.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import data from '../data/data';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Product from "../components/Product";
-
+import data from '../data/data';
 
 export default function HomePage() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () =>{
+      try{
+        setLoading(true);
+        const {data} = await axios.get('/api/products');
+        setLoading(false);
+        setProducts(data);
+      } catch (err){
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  },[])
     return (
         <div>
+          {/* {loading? (
+            <LoadingBox></LoadingBox>
+          ) : error ? (
+           <MessageBox>{error}</MessageBox>
+          ):( */}
           <div className="row center">
             {data.products.map(product => (
               <Product key={product._id} product={product}></Product>
             ))}           
           </div>
         </div>
-    )
+        );  
 }
